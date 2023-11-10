ee721795f86dabd547d5155e87499800eae89ae7
anghosts <2309744275@qq.com>
2023-11-10 14:54:52 +0800

    基础框架

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -1,21 +1,33 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+type ProfileType = {
+  nickname: string
+  token: string
+}
 
 // 定义 Store
 export const useMemberStore = defineStore(
   'member',
   () => {
     // 会员信息
-    const profile = ref<any>()
+    const profile: Ref<ProfileType> = ref({
+      nickname: '',
+      token: '',
+    })
 
     // 保存会员信息，登录时使用
-    const setProfile = (val: any) => {
+    const setProfile = (val: ProfileType) => {
       profile.value = val
     }
 
     // 清理会员信息，退出时使用
     const clearProfile = () => {
-      profile.value = undefined
+      profile.value = {
+        nickname: '',
+        token: '',
+      }
     }
 
     // 记得 return
@@ -25,8 +37,17 @@ export const useMemberStore = defineStore(
       clearProfile,
     }
   },
-  // TODO: 持久化
+  // 持久化
   {
-    persist: true,
+    persist: {
+      storage: {
+        getItem(key) {
+          return uni.getStorageSync(key)
+        },
+        setItem(key, value) {
+          uni.setStorageSync(key, value)
+        },
+      },
+    },
   },
 )
---
+++ src/utils/request.ts
@@ -0,0 +1,64 @@
+import { useMemberStore } from '@/stores'
+
+const BASE_URL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+const memberStore = useMemberStore()
+
+// 添加拦截器
+const interceptor = {
+  // 响应拦截器
+  invoke(options: UniApp.RequestOptions) {
+    // 设置请求超时时间
+    options.timeout = 10000
+    // 拼接地址
+    if (!options.url.startsWith('http')) {
+      options.url = BASE_URL + options.url
+    }
+    // 获取token
+    const { token } = memberStore.profile
+    // 设置请求头
+    options.header = {
+      ...options.header,
+      'source-client': 'miniapp',
+      Authoriztion: token,
+    }
+  },
+}
+uni.addInterceptor('request', interceptor)
+uni.addInterceptor('uploadFile', interceptor)
+
+type Data<T> = {
+  code: string
+  msg: string
+  result: T
+}
+
+// 封装请求
+export const request = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<Data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      success(res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          // 请求成功
+          resolve(res.data as Data<T>)
+        } else if (res.statusCode === 401) {
+          // 用户信息失效
+          memberStore.clearProfile()
+          uni.navigateTo({ url: '/pages/login/login' })
+          reject(res)
+        } else {
+          // 其他错误
+          uni.showToast({
+            title: (res.data as Data<T>).msg || '请求错误！',
+            icon: 'none',
+          })
+          reject(res)
+        }
+      },
+      fail(err) {
+        uni.showToast({ title: '网络错误', icon: 'error' })
+        reject(err)
+      },
+    })
+  })
+}

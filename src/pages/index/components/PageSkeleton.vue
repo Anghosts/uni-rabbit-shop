44e8c512e15149d64f0fa8348f48a77efbd41d08
anghosts <2309744275@qq.com>
2023-11-12 16:49:39 +0800

    首页-完成

---
+++ src/pages/index/components/PageSkeleton.vue
@@ -0,0 +1,483 @@
+<template>
+  <view is="components/BaseSwiper">
+    <view class="carousel BaseSwiper--carousel data-v-2f6dfe10 BaseSwiper--data-v-2f6dfe10">
+      <swiper circular class="data-v-2f6dfe10 BaseSwiper--data-v-2f6dfe10"></swiper>
+      <view class="indicator BaseSwiper--indicator data-v-2f6dfe10 BaseSwiper--data-v-2f6dfe10">
+        <text class="dot BaseSwiper--dot data-v-2f6dfe10 BaseSwiper--data-v-2f6dfe10"></text>
+        <text class="dot BaseSwiper--dot data-v-2f6dfe10 BaseSwiper--data-v-2f6dfe10"></text>
+        <text class="dot BaseSwiper--dot data-v-2f6dfe10 BaseSwiper--data-v-2f6dfe10"></text>
+      </view>
+    </view>
+  </view>
+  <view is="pages/index/components/CategoryPanel">
+    <view class="category CategoryPanel--category data-v-44ae5a81 CategoryPanel--data-v-44ae5a81">
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-72 sk-text"
+          >居家</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-706 sk-text"
+          >锦鲤</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-740 sk-text"
+          >服饰</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-526 sk-text"
+          >母婴</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-502 sk-text"
+          >个护</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-77 sk-text"
+          >严选</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-207 sk-text"
+          >数码</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-14 sk-text"
+          >运动</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-10 sk-text"
+          >杂项</text
+        >
+      </navigator>
+      <navigator
+        class="category-item CategoryPanel--category-item data-v-44ae5a81 CategoryPanel--data-v-44ae5a81"
+        hover-class="none"
+      >
+        <image
+          class="icon CategoryPanel--icon data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-image"
+        ></image>
+        <text
+          class="text CategoryPanel--text data-v-44ae5a81 CategoryPanel--data-v-44ae5a81 sk-transparent sk-text-14-2857-619 sk-text"
+          >品牌</text
+        >
+      </navigator>
+    </view>
+  </view>
+  <view is="pages/index/components/HotPanel">
+    <view class="panel HotPanel--panel hot HotPanel--hot data-v-df6c854a HotPanel--data-v-df6c854a">
+      <view class="item HotPanel--item data-v-df6c854a HotPanel--data-v-df6c854a">
+        <view class="title HotPanel--title data-v-df6c854a HotPanel--data-v-df6c854a">
+          <text
+            class="title-text HotPanel--title-text data-v-df6c854a HotPanel--data-v-df6c854a sk-transparent sk-text-14-2857-848 sk-text"
+            >特惠推荐</text
+          >
+          <text
+            class="title-desc HotPanel--title-desc data-v-df6c854a HotPanel--data-v-df6c854a sk-transparent sk-text-14-2857-693 sk-text"
+            >精选全攻略</text
+          >
+        </view>
+        <navigator
+          class="cards HotPanel--cards data-v-df6c854a HotPanel--data-v-df6c854a"
+          hover-class="none"
+        >
+          <image
+            class="image HotPanel--image data-v-df6c854a HotPanel--data-v-df6c854a sk-image"
+            mode="aspectFit"
+          ></image>
+          <image
+            class="image HotPanel--image data-v-df6c854a HotPanel--data-v-df6c854a sk-image"
+            mode="aspectFit"
+          ></image>
+        </navigator>
+      </view>
+      <view class="item HotPanel--item data-v-df6c854a HotPanel--data-v-df6c854a">
+        <view class="title HotPanel--title data-v-df6c854a HotPanel--data-v-df6c854a">
+          <text
+            class="title-text HotPanel--title-text data-v-df6c854a HotPanel--data-v-df6c854a sk-transparent sk-text-14-2857-249 sk-text"
+            >爆款推荐</text
+          >
+          <text
+            class="title-desc HotPanel--title-desc data-v-df6c854a HotPanel--data-v-df6c854a sk-transparent sk-text-14-2857-279 sk-text"
+            >最受欢迎</text
+          >
+        </view>
+        <navigator
+          class="cards HotPanel--cards data-v-df6c854a HotPanel--data-v-df6c854a"
+          hover-class="none"
+        >
+          <image
+            class="image HotPanel--image data-v-df6c854a HotPanel--data-v-df6c854a sk-image"
+            mode="aspectFit"
+          ></image>
+          <image
+            class="image HotPanel--image data-v-df6c854a HotPanel--data-v-df6c854a sk-image"
+            mode="aspectFit"
+          ></image>
+        </navigator>
+      </view>
+      <view class="item HotPanel--item data-v-df6c854a HotPanel--data-v-df6c854a">
+        <view class="title HotPanel--title data-v-df6c854a HotPanel--data-v-df6c854a">
+          <text
+            class="title-text HotPanel--title-text data-v-df6c854a HotPanel--data-v-df6c854a sk-transparent sk-text-14-2857-511 sk-text"
+            >一站买全</text
+          >
+          <text
+            class="title-desc HotPanel--title-desc data-v-df6c854a HotPanel--data-v-df6c854a sk-transparent sk-text-14-2857-500 sk-text"
+            >精心优选</text
+          >
+        </view>
+        <navigator
+          class="cards HotPanel--cards data-v-df6c854a HotPanel--data-v-df6c854a"
+          hover-class="none"
+        >
+          <image
+            class="image HotPanel--image data-v-df6c854a HotPanel--data-v-df6c854a sk-image"
+            mode="aspectFit"
+          ></image>
+          <image
+            class="image HotPanel--image data-v-df6c854a HotPanel--data-v-df6c854a sk-image"
+            mode="aspectFit"
+          ></image>
+        </navigator>
+      </view>
+      <view class="item HotPanel--item data-v-df6c854a HotPanel--data-v-df6c854a">
+        <view class="title HotPanel--title data-v-df6c854a HotPanel--data-v-df6c854a">
+          <text
+            class="title-text HotPanel--title-text data-v-df6c854a HotPanel--data-v-df6c854a sk-transparent sk-text-14-2857-846 sk-text"
+            >新鲜好物</text
+          >
+          <text
+            class="title-desc HotPanel--title-desc data-v-df6c854a HotPanel--data-v-df6c854a sk-transparent sk-text-14-2857-710 sk-text"
+            >生活加分项</text
+          >
+        </view>
+        <navigator
+          class="cards HotPanel--cards data-v-df6c854a HotPanel--data-v-df6c854a"
+          hover-class="none"
+        >
+          <image
+            class="image HotPanel--image data-v-df6c854a HotPanel--data-v-df6c854a sk-image"
+            mode="aspectFit"
+          ></image>
+          <image
+            class="image HotPanel--image data-v-df6c854a HotPanel--data-v-df6c854a sk-image"
+            mode="aspectFit"
+          ></image>
+        </navigator>
+      </view>
+    </view>
+  </view>
+  <view is="components/BaseGuess" class="r">
+    <view class="caption BaseGuess--caption data-v-0682eb28 BaseGuess--data-v-0682eb28"></view>
+  </view>
+</template>
+
+<style scoped>
+.sk-transparent {
+  color: transparent !important;
+}
+.sk-text-14-2857-183 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 39.2rpx;
+  position: relative !important;
+}
+.sk-text {
+  background-origin: content-box !important;
+  background-clip: content-box !important;
+  background-color: transparent !important;
+  color: transparent !important;
+  background-repeat: repeat-y !important;
+}
+.sk-text-28-1250-899 {
+  background-image: linear-gradient(
+    transparent 28.125%,
+    #eeeeee 0%,
+    #eeeeee 71.875%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 64rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-72 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-706 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-740 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-526 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-502 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-77 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-207 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-14 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-10 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-619 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-848 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 44.8rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-693 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-249 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 44.8rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-279 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-511 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 44.8rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-500 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-846 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 44.8rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-710 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-image {
+  background: #efefef !important;
+}
+.sk-pseudo::before,
+.sk-pseudo::after {
+  background: #efefef !important;
+  background-image: none !important;
+  color: transparent !important;
+  border-color: transparent !important;
+}
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+  border-radius: 0 !important;
+}
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+  border-radius: 50% !important;
+}
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
